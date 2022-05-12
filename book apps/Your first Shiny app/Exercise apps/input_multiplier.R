9e7a5bf0ce57634fdfe4336856075d3b60d18efb
Gmusebe <musebeivan@gmail.com>
2022-05-12 08:03:26 +0300

    exercise 3

--- book apps/Your first Shiny app/Exercise apps/input_multiplier.R
+++ book apps/Your first Shiny app/Exercise apps/input_multiplier.R
@@ -3,7 +3,7 @@ library(shiny)
 ui <- fluidPage(
   sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
   sliderInput("y", label = "and y is", min = 1, max = 50, value = 30),
-  "then x times 5 is",
+  "then (x * y) is",
   textOutput("product")
 )
 
