b704df17c36dc09072dc5550aabeeb0de17bfd06
Gmusebe <musebeivan@gmail.com>
2022-05-17 21:44:44 +0300

    reactivity

---
+++ book apps/Basic Reactivity/app.R
@@ -0,0 +1,16 @@
+# --Basic Reactivity--
+
+library(shiny)
+
+ui <- fluidPage(
+  textInput("name", "What is your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, session) {
+  output$greeting <- renderText({
+    paste0("Hello ", input$name, "!")
+  })
+}
+
+shinyApp(ui, server)
