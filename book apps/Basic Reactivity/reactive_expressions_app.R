3179f53142b9803af43f761bb20a9b6b613060a5
Gmusebe <musebeivan@gmail.com>
2022-05-24 23:14:08 +0300

    reactive expressions

--- book apps/Basic Reactivity/reactive_expressions_app.R
+++ book apps/Basic Reactivity/reactive_expressions_app.R
@@ -30,9 +30,6 @@ t_test <- function(x1, x2) {
   )
 }
 
-freqpoly(x1, x2)
-cat(t_test(x1, x2))
-
 ui <- fluidPage(
   fluidRow(
     column(4,
@@ -60,6 +57,30 @@ ui <- fluidPage(
 )
   
 server <- function(input, output, sessions){
+  freqpoly <- function(x1, x2, bindwidth = 0.1, xlim = c(-3, 3)) {
+    df <- data.frame(
+      x <- c(x1, x2),
+      g <- c(
+        rep("x1", length(x1)),
+        rep("x2", length(x2))
+      )
+    )
+    
+    ggplot(df,aes(x, colour = g)) +
+      geom_freqpoly(bindwidth = bindwidth, size = 1) +
+      coord_cartesian(xlim = xlim)
+  }
+  
+  t_test <- function(x1, x2) {
+    test <- t.test(x1, x2)
+    
+    # use sprintf() to format t.test() results compactly
+    sprintf(
+      "p value: %0.3f\n[%0.2f, %0.2f]",
+      test$p.value, test$conf.int[1], test$conf.int[2]
+    )
+  }
+  
   output$hist <- renderPlot({
     x1 <- rnorm(input$n1, input$mean1, input$sd1)
     x2 <- rnorm(input$n2, input$mean2, input$sd2)
