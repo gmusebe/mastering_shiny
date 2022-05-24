a3c5ab384cc0f917e1d48aa22a32abb916696db1
Gmusebe <musebeivan@gmail.com>
2022-05-24 23:09:05 +0300

    reactive expressions

--- book apps/Basic Reactivity/reactive_expressions_app.R
+++ book apps/Basic Reactivity/reactive_expressions_app.R
@@ -6,9 +6,6 @@
 library(shiny)
 library(ggplot2)
 
-x1 <- rnorm(100, mean = 0, sd = 0.5)
-x2 <- rnorm(200, mean = 0.15, sd = 0.9)
-
 freqpoly <- function(x1, x2, bindwidth = 0.1, xlim = c(-3, 3)) {
   df <- data.frame(
     x <- c(x1, x2),
@@ -33,6 +30,52 @@ t_test <- function(x1, x2) {
   )
 }
 
-
 freqpoly(x1, x2)
 cat(t_test(x1, x2))
+
+ui <- fluidPage(
+  fluidRow(
+    column(4,
+      "Distribution 1",
+      numericInput("n1", label = "n", value = 1000, min = 1),
+      numericInput("mean1", label = "µ", value = 0, step = 0.1),
+      numericInput("sd1", label = "σ", value = 0.5, min =  0.1, step = 0.1)
+    ),
+    column(4,
+           "Distribution 2",
+           numericInput("n2", label = "n", value = 1000, min = 1),
+           numericInput("mean2", label = "µ", value = 0, step = 0.1),
+           numericInput("sd2", label = "σ", value = 0.5, min =  0.1, step = 0.1)
+    ),
+    column(4,
+      "Frequency Polygon",
+      numericInput("bindwidth", label = "Bin Width", value = 0.1, step = 0.1),
+      sliderInput("range", label = "range", value = c(-3, 3), min = -5, max = 5)
+    )
+  ),
+  fluidRow(
+    column(9, plotOutput("hist")),
+    column(3, verbatimTextOutput("ttest"))
+  )
+)
+  
+server <- function(input, output, sessions){
+  output$hist <- renderPlot({
+    x1 <- rnorm(input$n1, input$mean1, input$sd1)
+    x2 <- rnorm(input$n2, input$mean2, input$sd2)
+    
+    freqpoly(
+      x1, x2, 
+      bindwidth = input$bindwidth, xlim = input$range
+    )
+  }, res = 96)
+  
+  output$ttest <- renderText({
+    x1 <- rnorm(input$n1, input$mean1, input$sd1)
+    x2 <- rnorm(input$n2, input$mean2, input$sd2)
+    
+    t_test(x1, x2)
+  })
+}
+
+shinyApp(ui, server)
