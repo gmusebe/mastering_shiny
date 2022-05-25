3dd5a2dd690c6f93703e0a651dba0d37b1caba4b
Gmusebe <musebeivan@gmail.com>
2022-05-25 07:32:03 +0300

    ctr_time

--- book apps/Basic Reactivity/action_button.R
+++ book apps/Basic Reactivity/action_button.R
@@ -1,21 +1,21 @@
 # -- Controlling timing of evaluation --
 library(shiny)
 library(ggplot2)
- 
+
 ui <- fluidPage(
   fluidRow(
     column(3, 
            numericInput("lambda1", label = "lambda1", value = 3),
            numericInput("lambda2", label = "lambda2", value = 5),
-           numericInput("n", label = "n", value = 1e4, min = 0)
+           numericInput("n", label = "n", value = 1e4, min = 0),
+           numericInput("bindwidth", label = "Bin Width", value = 0.1, step = 0.1),
+           actionButton("simulate", "Simulate!")
     ),
     column(9, plotOutput("hist"))
   )
 )
 
 server <- function(input, output, session) {
-  timer <- reactiveTimer(500)
-  
   freqpoly <- function(x1, x2, bindwidth = 0.1, xlim = c(-3, 3)) {
     df <- data.frame(
       x <- c(x1, x2),
@@ -31,16 +31,15 @@ server <- function(input, output, session) {
   }
   
   x1 <- reactive({
-    timer()
+    input$simulate
     rpois(input$n, input$lambda1)
   })
   x2 <- reactive({
-    timer()
+    input$simulate
     rpois(input$n, input$lambda2)
   })
-  
   output$hist <- renderPlot({
-    freqpoly(x1(), x2(), bindwidth = 1, xlim = c(0, 40))
+    freqpoly(x1(), x2(), bindwidth = input$bindwidth, xlim = c(0, 40))
   }, res = 96)
 }
 
